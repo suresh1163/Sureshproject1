0074a47086406fb9b24cfd6c6613118ad5f62ef9
suresh113 <sureshk.1163@gmail.com>
2019-09-11 14:34:29 +0530

    java

---
+++ src/test/java/suresh/Fibanacci.java
@@ -0,0 +1,19 @@
+package suresh;
+
+public class Fibanacci {
+		public static void main (String []args)
+		{
+			int a=0,b=1;
+			System.out.println(a);
+			System.out.println(b);
+			for(int i=0;i<=10;i++)
+			{
+				int c=a+b;
+				System.out.println(c);
+				a=b;
+				b=c;
+			}
+		}
+
+	}
+
